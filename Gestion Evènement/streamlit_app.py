721cd95d2504712bc9da1219b65892e1d3984f3b
nasriham <nasri_hamza@outlook.com>
2026-06-03 15:52:33 +0200

    Add files via upload

--- Gestion Evènement/streamlit_app.py
+++ Gestion Evènement/streamlit_app.py
@@ -12,6 +12,7 @@ page = st.navigation([
     st.Page("pages/accueil.py", title="Accueil", icon="🏠"),
     st.Page("pages/evenements_externes.py", title="Événements externes", icon="📋"),
     st.Page("pages/evenements_techniques.py", title="Événements techniques", icon="🔧"),
+    st.Page("pages/travaux.py", title="Travaux", icon="🏗️"),
     st.Page("pages/occupation_parking.py", title="Impact Parking", icon="⚠️"),
 ])
 
